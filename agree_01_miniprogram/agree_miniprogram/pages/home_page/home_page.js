9dbe4e8883c509b0768367135fdd71e43c5f6054
xuzhuojie <xuzhuojie@agree.com.cn>
2021-10-20 10:57:41 +0800

    修改homepage图片路径

--- agree_01_miniprogram/agree_miniprogram/pages/home_page/home_page.js
+++ agree_01_miniprogram/agree_miniprogram/pages/home_page/home_page.js
@@ -5,6 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
+    url:{
+      url_homepage: getApp().globalData.img_service + "/miniprogramImg/img_homepage"
+    },
     products: [{
       name: '活期理财',
       desp: '代销招赢开鑫…',
@@ -26,53 +29,64 @@ Page({
         // callback:this.appointmentAccountOpening,
         id: "1",
         text: "预约开户",
-        icon: "https://www.aweb.org.cn/static/ui/poc/yuyuekaihu.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/yuyuekaihu.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/yuyuekaihu.png",
       },
       {
         id: "2",
         text: "开卡",
-        icon: "https://www.aweb.org.cn/static/ui/poc/kaika.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/kaika.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/kaika.png",
       },
       {
         id: "3",
         text: "转账",
-        icon: "https://www.aweb.org.cn/static/ui/poc/zhuanzhang.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/zhuanzhang.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/zhuanzhang.png",
       },
       {
         id: "4",
         text: "理财",
-        icon: "https://www.aweb.org.cn/static/ui/poc/zhuanzhang.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/zhuanzhang.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/zhuanzhang.png",
       },
       {
         id: "5",
         text: "电子回单",
-        icon: "https://www.aweb.org.cn/static/ui/poc/dianzihuidan.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/dianzihuidan.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/dianzihuidan.png",
       },
       {
         id: "6",
         text: "缴费",
-        icon: "https://www.aweb.org.cn/static/ui/poc/jiaofei.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/jiaofei.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/jiaofei.png",
       },
       {
         id: "7",
         text: "贷款",
-        icon: "https://www.aweb.org.cn/static/ui/poc/daikuan.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/daikuan.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/daikuan.png",
       },
       {
         id: "8",
         text: "全部",
-        icon: "https://www.aweb.org.cn/static/ui/poc/quanbu.png",
+        // icon: "https://www.aweb.org.cn/static/ui/poc/quanbu.png",
+        icon: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/quanbu.png",
       },
     ],
     info: [
       {
-        url: "https://www.aweb.org.cn/static/ui/poc/banner.png",
+        // url: "https://www.aweb.org.cn/static/ui/poc/banner.png",
+        url: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/bannner.png",
       },
       {
-        url: "https://www.aweb.org.cn/static/ui/poc/banner.png",
+        // url: "https://www.aweb.org.cn/static/ui/poc/banner.png",
+        url: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/bannner.png",
       },
       {
-        url: "https://www.aweb.org.cn/static/ui/poc/banner.png",
+        // url: "https://www.aweb.org.cn/static/ui/poc/banner.png",
+        url: getApp().globalData.img_service + "/miniprogramImg/img_homepage" + "/bannner.png",
       },
     ],
     current: 0,
@@ -86,7 +100,6 @@ Page({
       selectedBorder: "#407FFF",
     },
   },
-
   // 点击取号按钮事件
   bt_take_number: function () {
     wx.navigateTo({
@@ -97,9 +110,14 @@ Page({
     console.log("点击打开交易：",event)
     // 开卡交易 id 在data设置为2
     console.log("id:", event.currentTarget.dataset.id)
-    if(event.currentTarget.dataset.id){
+    if(event.currentTarget.dataset.id == "2"){
       wx.navigateTo({
         url: '/pages/bank_card/bc_index/bc_index',
+      })
+    }else{
+      wx.showToast({
+        title: '未开通该服务',
+        icon: "error"
       })
     }
   },
@@ -113,17 +131,37 @@ Page({
   },
   gridChange(e) {
     let { detail } = e;
-    console.log(e);
+    // console.log(e);
     let index = detail && detail.index;
-    console.log(index)
+    // console.log(index)
     let item = this.list[index];
-    console.log(item)
-    console.log(item.callback)
+    // console.log(item)
+    // console.log(item.callback)
     item && item.callback && item.callback(item)
   },
   change(e) {
     this.current = e.detail.current;
   },
+
+  // 请求token
+  getToken: function () {
+    let this_page = this
+    wx.request({
+      url: 'https://www.xiongweixp.tech/adaas/token?username=admin&password=admin&grant_type=password&scope=device&client_id=client_password&client_secret=123456', //仅为示例，并非真实的接口地址
+      method: "POST",
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res)
+        let app = getApp()
+        app.globalData.token = res.data.access_token
+        this_page.data.token = res.data.access_token
+        console.log("取号 token : ", app.globalData.token)
+      }
+    })
+  },
+  // ----------------------------------------------------------------------------------------
   /**
   * 生命周期函数--监听页面加载
   */
